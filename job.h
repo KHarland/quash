38563ecb67f30d32967458b2b5bbf005a54730b4
Kendal Harland <kharland2011@gmail.com>
2014-03-25 16:30:16 -0500

    adding job.h

--- job.h
+++ job.h
@@ -4,9 +4,29 @@
 
 struct Job
 {
-	char *qargv[MAX_ARGS];
-	int qargc;
-	int jid;
+	char *argv[MAX_ARGS];
+	int argc;
+	int id;
+	bool bg;
+	bool alive;
+
+	Job()
+	{
+		for(int i=0; i<MAX_ARGS; i++)
+			argv[i] = new char[256];
+
+		argc = 0;
+		id = 0;
+		bg = false;
+		alive = false;
+	}
+
+	~Job()
+	{
+		//deallocate variables and exit quash
+		for(int i=0; i<MAX_ARGS; i++)
+			delete [] argv[i];
+	}
 };
 
 #endif//JOB_H
